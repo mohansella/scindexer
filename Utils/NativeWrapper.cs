59740fbfa0f0f1cf3b1e25dd9d48443a3ccc9f1e
mohansella <rkssmohankumar@gmail.com>
2020-07-25 13:41:26 +0530

    native wrapper added

---
+++ Utils/NativeWrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SCIndexer.Utils
+{
+    public class NativeWrapper
+    {
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern SafeFileHandle CreateFile(
+           string lpFileName,
+           [MarshalAs(UnmanagedType.U4)] FileAccess dwDesiredAccess,
+           [MarshalAs(UnmanagedType.U4)] FileShare dwShareMode,
+           IntPtr lpSecurityAttributes,
+           [MarshalAs(UnmanagedType.U4)] FileMode dwCreationDisposition,
+           [MarshalAs(UnmanagedType.U4)] FileAttributes dwFlagsAndAttributes,
+           IntPtr hTemplateFile);
+
+    }
+}
