9c2d5f5806824ab4d5e75472acff651918234431
mohansella <rkssmohankumar@gmail.com>
2020-08-01 22:36:49 +0530

    indexer file added

---
+++ Indexer/ElasticIndexer.cs
@@ -0,0 +1,36 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCIndexer.Indexer
+{
+    public class ElasticIndexer : Indexer
+    {
+
+        public static readonly string DefaultHostName = "localhost";
+        public static readonly int DefaultPortNumber = 9200;
+
+        private ElasticClient client = null;
+
+        public ElasticIndexer(string hostName = null, int? portNumber = null)
+        {
+            hostName = hostName == null ? DefaultHostName : hostName;
+            portNumber = portNumber == null ? DefaultPortNumber : portNumber;
+            var node = new Uri($"http://{hostName}:{portNumber}");
+            var config = new ConnectionSettings(node);
+            this.client = new ElasticClient(config);
+        }
+
+        public void CreateIndex(string indexName)
+        {
+            this.client.Indices.Create(indexName);
+        }
+
+        public bool IsIndexExists(string indexName)
+        {
+            return this.client.Indices.Exists(indexName).Exists;
+        }
+    }
+}
---
+++ Indexer/Indexer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCIndexer.Indexer
+{
+    public interface Indexer
+    {
+
+        public bool IsIndexExists(string indexName);
+
+        public void CreateIndex(string indexName);
+
+    }
+}
